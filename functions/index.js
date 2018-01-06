2621ad9285ff396b365bc77d8bf4fa9947097cc5
Connor Grieb <congrieb@umich.edu>
2018-01-05 22:55:50 -0500

    update action name to be single_resort instead of make_name

--- functions/index.js
+++ functions/index.js
@@ -5,21 +5,14 @@ const App = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 
 
-// a. the action name from the make_name Dialogflow intent
-const NAME_ACTION = 'make_name';
-// b. the parameters that are parsed from the make_name intent 
-const COLOR_ARGUMENT = 'color';
-const NUMBER_ARGUMENT = 'number';
-
-
-exports.sillyNameMaker = functions.https.onRequest((request, response) => {
+exports.PowderHunter = functions.https.onRequest((request, response) => {
   const app = new App({request, response});
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
 
 
 // c. The function that generates the silly name
-  function makeName (app) {
+  function singleResort (app) {
     var snow = require('snow-forecast-sfr');
     var result;
     let resort_name = app.getArgument('resort_name');
@@ -30,7 +23,7 @@ exports.sillyNameMaker = functions.https.onRequest((request, response) => {
   }
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
-  actionMap.set(NAME_ACTION, makeName);
+  actionMap.set('single_resort', singleResort);
 
 
 app.handleRequest(actionMap);
