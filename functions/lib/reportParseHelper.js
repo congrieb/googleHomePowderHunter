02823b0083ec8d0c4673b223e0ee5a781a799c1e
Connor Grieb <congrieb@umich.edu>
2018-01-24 20:52:06 -0500

    adding some comments

--- functions/lib/reportParseHelper.js
+++ functions/lib/reportParseHelper.js
@@ -2,6 +2,7 @@ function ReportParseHelper() {
     this.mSecondPerDay = 1000 * 60 * 60 * 24;
 }
 
+// Returns the index of the element in the array for which the element with "key" is the greatest
 ReportParseHelper.prototype.findIndexOfMaxValue = function(array, key) {
     var maxValue = -1;
     var maxIndex = -1;
@@ -17,6 +18,7 @@ ReportParseHelper.prototype.findIndexOfMaxValue = function(array, key) {
     return maxIndex;
 };
 
+// Convert the forecast from having an array entry for morning noon and night everyday, to having one entry per day with that days snow total
 ReportParseHelper.prototype.makeReportDaily = function(forecast) {
     var dailyForecast = [];
     for(var i = 0; i < forecast.length; i++) {
